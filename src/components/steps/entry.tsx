7fab8ab18994a96f6919b6180f612d005a4ad31f
Morgane <morgane.gamory@nockee.fr>
2024-02-08 15:12:07 +0100

    fix: last details

--- src/components/steps/entry.tsx
+++ src/components/steps/entry.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@chakra-ui/react';
+import { Box, Center, Stack } from '@chakra-ui/react';
 
 import CustomButton from '../common/custom_button';
 
@@ -8,8 +8,6 @@ interface EntryProps {
 }
 
 export default function Entry({ next, handleEnd }: EntryProps) {
-  const isCompleted = localStorage.getItem('completed');
-
   return (
     <Stack textAlign='justify' spacing={4}>
       <Box>
@@ -27,15 +25,9 @@ export default function Entry({ next, handleEnd }: EntryProps) {
         Préparez-vous à plonger dans un monde où chaque détail a son importance.
         Êtes-vous prêt à relever le défi ?
       </Box>
-      <Stack alignItems='center' my={10}>
+      <Center my={6}>
         <CustomButton action={next} text="C'est parti !" />
-        {isCompleted && (
-          <CustomButton
-            action={handleEnd}
-            text='Je connais déjà cette histoire, amenez moi sur la Montagne.'
-          />
-        )}
-      </Stack>
+      </Center>
       <Box as='i' fontSize='sm'>
         À savoir : Chaque fois que l&apos;on vous demandera la solution
         d&apos;une énigme, au bout de 2 essais vous aurez un indice, au bout de
